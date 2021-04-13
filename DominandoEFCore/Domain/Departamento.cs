963f5303e644796fafea5592c8e5f81bc2e00a00
Leonardo Souza <leonardo.souza@lambda3.com.br>
2021-04-13 01:32:21 -0300

    criando relacionamento - props nas classes

--- DominandoEFCore/Domain/Departamento.cs
+++ DominandoEFCore/Domain/Departamento.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace DominandoEFCore.Domain
 {
     public class Departamento
@@ -5,5 +7,8 @@ namespace DominandoEFCore.Domain
         public int Id { get; set; }
         public string Descricao { get; set; }
         public bool Ativo { get; set; }
+
+        // prop. de navegação (1 departamento tem muitos funcionários)
+        public List<Funcionario> Funcionarios { get; set; }
     }
 }
--- DominandoEFCore/Domain/Funcionario.cs
+++ DominandoEFCore/Domain/Funcionario.cs
@@ -5,5 +5,10 @@ namespace DominandoEFCore.Domain
         public int Id { get; set; }
         public string Nome { get; set; }
         public string CPF { get; set; }
+
+        // relacionamento (1 departamento tem muitos funcionários
+        // e 1 funcionário possui 1 departamento - n:1)
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; } // prop. de navegação
     }
 }
