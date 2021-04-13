07c85c6bd25544a14862976d7e41507dee410082
Leonardo Souza <leonardo.souza@lambda3.com.br>
2021-04-13 01:26:16 -0300

    criando classes de dommínio

---
+++ DominandoEFCore/Domain/Departamento.cs
@@ -0,0 +1,9 @@
+namespace DominandoEFCore.Domain
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ DominandoEFCore/Domain/Funcionario.cs
@@ -0,0 +1,9 @@
+namespace DominandoEFCore.Domain
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
